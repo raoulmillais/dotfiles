07f289e9bba1b6fa9493f0ba57ff859e9820fa59
Raoul Millais <raoul@raoulmillais.com>
2022-05-20 17:32:48 +0100

    Config add telescope-fzf-native

--- .config/nvim/lua/raoulmillais/plug.lua
+++ .config/nvim/lua/raoulmillais/plug.lua
@@ -44,7 +44,10 @@ return require("packer").startup(function(use)
     'nvim-telescope/telescope.nvim',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
- 
+  use {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    run = 'make'
+  }
   --
   -- Legacy
   --
