8f33b0f5533d0b2416b0b802ee7ff25808a82a2e
Raoul Millais <raoul@raoulmillais.com>
2022-05-23 13:06:07 +0100

    Rounded borders on cmp menu

--- .config/nvim/lua/raoulmillais/cmp/init.lua
+++ .config/nvim/lua/raoulmillais/cmp/init.lua
@@ -15,6 +15,10 @@ cmp.setup {
       })
     },
   },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
   snippet = {
     expand = function(args)
       require("luasnip").lsp_expand(args.body)
