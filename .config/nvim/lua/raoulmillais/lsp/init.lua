860ff78ed1d1e23759748cddcb22a3f7580c0307
Raoul Millais <raoul@raoulmillais.com>
2022-06-01 16:10:21 +0100

    Add vim.diagnostic config

--- .config/nvim/lua/raoulmillais/lsp/init.lua
+++ .config/nvim/lua/raoulmillais/lsp/init.lua
@@ -101,6 +101,14 @@ require("fidget").setup {
   },
 }
 
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = true,
+  severity_sort = false,
+})
+
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
