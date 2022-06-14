86c3545159e00f44fef2e1f5d4b2d4dfcd7c8b00
Raoul Millais <raoul@raoulmillais.com>
2022-06-14 16:10:32 +0100

    Remove NERDTree config

--- .config/nvim/lua/raoulmillais/plugins.lua
+++ .config/nvim/lua/raoulmillais/plugins.lua
@@ -7,12 +7,6 @@ vim.g.iced_enable_default_key_mappings = true
 vim.g.sexp_enable_insert_mode_mappings = 0
 -- }}}
 
--- NERDTREE {{{1
-vim.g.NERDTreeIgnore = { "~$", ".git" }
-vim.g.NERDTreeMinimalUI = 1
-vim.g.NERDTreeDirArrows = 1
--- }}}
-
 -- DEVICONS {{{1
 vim.g.webdevicons_enable_nerdtree = 1
 -- after a re-source, fix syntax matching issues (concealing brackets):
