9e52bb5fe2432abc8f8aa6dff7dc5391d4dcf0bc
Raoul Millais <raoul@raoulmillais.com>
2022-11-09 18:00:21 +0000

    Tab completion with cmp - can't lose the muscle memory!

--- .config/nvim/lua/raoulmillais/cmp/init.lua
+++ .config/nvim/lua/raoulmillais/cmp/init.lua
@@ -30,7 +30,20 @@ cmp.setup {
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.abort(),
-    ["<tab>"] = cmp.config.disable,
+    ["<Tab>"] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end,
+    ["<S-Tab>"] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end,
   },
   sources = cmp.config.sources({
     { name = "gh_issues" },
