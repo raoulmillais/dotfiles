257164e711ca59449d7871067007b091524a7a37
Raoul Millais <raoul@raoulmillais.com>
2024-09-22 22:49:33 +0100

    Nvim: setup noice and add telescope keymaps

--- .config/nvim/lua/config/telescope/init.lua
+++ .config/nvim/lua/config/telescope/init.lua
@@ -24,21 +24,36 @@ telescope.setup {
       },
     },
   },
-  scroll_strategy = "cycle"
+  scroll_strategy = "cycle",
+  keys = {
+      -- find
+      { "<leader>fb", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>", desc = "Buffers" },
+      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files (Root Dir)" },
+      { "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Find Files (git-files)" },
+      { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
+      -- git
+      { "<leader>gc", "<cmd>Telescope git_commits<CR>", desc = "Commits" },
+      { "<leader>gs", "<cmd>Telescope git_status<CR>", desc = "Status" },
+      -- search
+      { '<leader>s"', "<cmd>Telescope registers<cr>", desc = "Registers" },
+      { "<leader>sa", "<cmd>Telescope autocommands<cr>", desc = "Auto Commands" },
+      { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer" },
+      { "<leader>sc", "<cmd>Telescope command_history<cr>", desc = "Command History" },
+      { "<leader>sC", "<cmd>Telescope commands<cr>", desc = "Commands" },
+      { "<leader>sd", "<cmd>Telescope diagnostics bufnr=0<cr>", desc = "Document Diagnostics" },
+      { "<leader>sD", "<cmd>Telescope diagnostics<cr>", desc = "Workspace Diagnostics" },
+      { "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "Help Pages" },
+      { "<leader>sH", "<cmd>Telescope highlights<cr>", desc = "Search Highlight Groups" },
+      { "<leader>sj", "<cmd>Telescope jumplist<cr>", desc = "Jumplist" },
+      { "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps" },
+      { "<leader>sl", "<cmd>Telescope loclist<cr>", desc = "Location List" },
+      { "<leader>sM", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
+      { "<leader>sm", "<cmd>Telescope marks<cr>", desc = "Jump to Mark" },
+      { "<leader>so", "<cmd>Telescope vim_options<cr>", desc = "Options" },
+      { "<leader>sR", "<cmd>Telescope resume<cr>", desc = "Resume" },
+      { "<leader>sq", "<cmd>Telescope quickfix<cr>", desc = "Quickfix List" },
+  }
 }
 
 telescope.load_extension('fzf')
 telescope.load_extension('ui-select')
-
-nmap('<leader>td', builtin.diagnostics)
-nmap('<leader>ta', vim.lsp.buf.code_action)
-nmap('<leader>tgs', finders.git_status)
-
--- Find
-nmap('<leader>ff', builtin.find_files)
-nmap('<leader>fg', builtin.live_grep)
-nmap('<leader>fb', builtin.buffers)
-nmap('<leader>fh', builtin.help_tags)
-
-nmap('<leader>fi', finders.find_file_in_project)
-nmap('<leader>fn', finders. find_nvim_config_file)
