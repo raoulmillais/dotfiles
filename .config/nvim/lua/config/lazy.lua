91c1060eccba4333b0e2dde1dc3277adaaf71a1a
Raoul Millais <raoul@raoulmillais.com>
2024-09-23 15:56:20 +0100

    Nvim: tidy up global init into config/lazy.lua

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -1,17 +1,54 @@
--- BUNDLED PLUGINS {{{1
--- Use the matchit macro to enable switching between open close tags and
--- if/elsif/else/end with %
-vim.cmd[[runtime macros/matchit.vim]]
-vim.cmd[[runtime ftplugin/man.vim]]  -- Enable viewing man pages
--- }}}
---
-local status_ok, lazy = pcall(require, "lazy")
-if not status_ok then
-  -- Should not get here - lazy-install.lua should run first
-  print("Lazy not installed")
-  return
+-- [
+-- Ensure lazy is installed
+-- ]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.uv.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
+
+-- [
+-- Configure lazy
+-- ]
+local lazy = require('lazy')
+
+-- These options must be set right at the start before lazy
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
 
 lazy.setup({
-  { import = "plugins" },
+  spec = { import = "plugins" },
+  performance = {
+    cache = { enabled = true },
+    -- reset the package path to improve startup time
+    reset_packpath = true,
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "matchparen",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  }
 })
+
+-- [
+-- Configure neovim
+-- ]
+require 'config.options'
+require 'config.autocommands'
+require 'config.keymaps'
+require 'config.lsp'
+require 'config.telescope'
+require 'config.diagnostics'
