0c9c4cf85a4e1235f6ab17d01a755ac546980a1a
Raoul Millais <raoul@raoulmillais.com>
2024-09-23 15:07:17 +0100

    Nvim: merge exec helper into telescope finders

--- .config/nvim/lua/config/telescope/finders.lua
+++ .config/nvim/lua/config/telescope/finders.lua
@@ -1,7 +1,28 @@
 local builtin = require('telescope.builtin')
 local tt = require('telescope.themes')
 local themes = require('config.telescope.themes')
-local exec = require('config.exec')
+local Job = require "plenary.job"
+
+-- Adapted from https://github.com/nvim-telescope/telescope.nvim/blob/54be102e20ee4acaaa17e9fce8be07fb586630df/lua/telescope/utils.lua#L390
+local function exec(cmd, cwd)
+  if type(cmd) ~= "table" then
+    vim.notify("Command has to be a table",  "ERROR")
+    return {}
+  end
+  local command = table.remove(cmd, 1)
+  local stderr = {}
+  local stdout, ret = Job
+    :new({
+      command = command,
+      args = cmd,
+      cwd = cwd,
+      on_stderr = function(_, data)
+        table.insert(stderr, data)
+      end,
+    })
+    :sync()
+  return stdout, ret, stderr
+end
 
 local finders =  {}
 
