c0a75864d8a0eac76e0e2a58eb03b7d275639492
Raoul Millais <raoul@raoulmillais.com>
2025-05-28 14:41:57 +0100

    nvim: turn on signature help in blink.cmp

--- .config/nvim/lua/plugins/blink.cmp.lua
+++ .config/nvim/lua/plugins/blink.cmp.lua
@@ -8,6 +8,7 @@ return {
       nerd_font_variant = 'mono'
     },
     completion = { documentation = { auto_show = false } },
+    signature = { enabled = true },
     sources = {
       default = { 'lsp', 'path', 'snippets', 'buffer' },
     },
