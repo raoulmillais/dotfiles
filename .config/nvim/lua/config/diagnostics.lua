0626fd716b27c8194a43a16c26c8a58279792983
Raoul Millais <raoul@raoulmillais.com>
2025-06-02 15:15:33 +0100

    nvim: add pending diag float fix when blink is open and up

--- .config/nvim/lua/config/diagnostics.lua
+++ .config/nvim/lua/config/diagnostics.lua
@@ -3,8 +3,55 @@ local keymaps = require('config.keymaps')
 
 c.register_normal_keymaps(keymaps.diagnostics)
 
+--[[
+-- These 3 functions push the diagnostic float out of the
+-- way of the blink completion menu when its open.
+--
+-- Sadly, we have to wait for a fix to this bug to be merged:
+-- https://github.com/neovim/neovim/issues/31573
+-- https://github.com/neovim/neovim/pull/31577/commits/5bb11844b3f73c981c5d090caa07d4f9444fe5c1o
+--]]
+---@diagnostic disable-next-line: unused-local, unused-function
+local function is_blink_menu_open_and_above_cursor()
+  local menu_win = require('blink.cmp.completion.windows.menu').win
+  if not menu_win then return false end
+
+  local menu_winnr = menu_win:get_win()
+  if not menu_winnr then return false end
+
+  local menu_win_config = vim.api.nvim_win_get_config(menu_winnr)
+  local cursor_win_row = vim.fn.winline()
+
+  return menu_win_config.row - cursor_win_row < 0
+end
+
+---@diagnostic disable-next-line: unused-local, unused-function
+local function get_blink_menu_width()
+  local menu_win = require('blink.cmp.completion.windows.menu').win
+  if not menu_win then return false end
+
+  return menu_win:get_width()
+end
+
+---@diagnostic disable-next-line: unused-local, unused-function
+local function diagnostics_float(_, _)
+  local opts = {
+    header = "",
+    source = true,
+    focusable = false,
+    anchor_bias = "above",
+  }
+  if is_blink_menu_open_and_above_cursor() then
+    local blink_menu_width = get_blink_menu_width()
+    opts.offset_x = blink_menu_width + 1
+  end
+
+  return opts
+end
+
 vim.diagnostic.config({
-  virtual_text = false, -- We show diagnostics in a float -- see autocmds.lua
+  virtual_text = false, -- We show diagnostics in a float
+  -- TODO: use `diagnostics_float` instead
   float = {
     header = "",
     source = true,
