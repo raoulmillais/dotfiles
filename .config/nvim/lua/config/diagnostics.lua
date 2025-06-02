4e9e61ea89597fcfdbfaf7580046226255d39298
Raoul Millais <raoul@raoulmillais.com>
2025-06-02 13:17:09 +0100

    nvim: improve dagnostics float window
    - No heading
    - Better default position when blink is open below (still shit when its
      above)
    - Make it non-focusable

--- .config/nvim/lua/config/diagnostics.lua
+++ .config/nvim/lua/config/diagnostics.lua
@@ -4,7 +4,13 @@ local keymaps = require('config.keymaps')
 c.register_normal_keymaps(keymaps.diagnostics)
 
 vim.diagnostic.config({
-  virtual_text = true,
+  virtual_text = false, -- We show diagnostics in a float -- see autocmds.lua
+  float = {
+    header = "",
+    source = true,
+    focusable = false,
+    anchor_bias = "above",
+  },
   signs = {
     text = {
       [vim.diagnostic.severity.ERROR] = " ",
