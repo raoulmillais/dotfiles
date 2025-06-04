e3bf73e32fd187a0629b213d1c1b781adf6faa80
Raoul Millais <raoul@raoulmillais.com>
2025-06-04 14:43:57 +0100

    nvim: temp telescope fix for winborder
    - Tidy up unneeded usage of plenary
    - Better config for telescope pickers

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -7,10 +7,13 @@ return {
   },
   { 'nvim-telescope/telescope-ui-select.nvim' },
   {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
     dependencies = {
       'nvim-telescope/telescope-fzf-native.nvim',
-      'nvim-lua/plenary.nvim'
+        -- Until plenary has the fix taking into account the new winborder
+        -- setting we have to use the fork with the pending PR
+        -- https://github.com/nvim-telescope/telescope.nvim/issues/3436
+      { 'emmanueltouzery/plenary.nvim', branch = 'winborder' }
     },
     opts = {
       color_devicons = false,
@@ -18,24 +21,28 @@ return {
         winblend = 0,
       },
       scroll_strategy = "cycle",
+      pickers = {
+        find_files = {
+          theme = "dropdown",
+          previewer = false,
+        }
+      },
+      extensions = {
+        ["ui-select"] = {
+          theme = "dropdown",
+          previewer = false,
+        },
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+          case_mode = "smart_case",
+        },
+      },
     },
     config = function(_, o)
       local telescope = require('telescope')
-      local themes = require('telescope.themes')
-      local merged_opts = c.merge(o, {
-        extensions = {
-          ["ui-select"] = {
-            themes.get_dropdown{}
-          },
-          fzf = {
-            fuzzy = true,
-            override_generic_sorter = true,
-            override_file_sorter = true,
-            case_mode = "smart_case",
-          },
-        },
-      })
-      telescope.setup(merged_opts)
+      telescope.setup(o)
       telescope.load_extension('fzf')
       telescope.load_extension('ui-select')
       local keymaps = require('config.keymaps')
--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,7 +1,6 @@
 local c = require('core')
 
 return {
-  { "nvim-lua/plenary.nvim"},
   {
     "nvim-tree/nvim-web-devicons" ,
     opts = {
@@ -28,7 +27,6 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     dependencies = {
-     "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
     },
     opts = {
